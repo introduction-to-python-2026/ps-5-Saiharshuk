da5783c4694b23a79b4dc07e3725f30e4c98e2fe
Saiharshuk <sai.harshuk@live.biu.ac.il>
2025-11-23 18:01:38 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,11 +1,41 @@
 
 
 
+
 def split_before_uppercases(formula):
-    pass  # replace the pass with your code
+# Handle empty string
+if formula == "":
+    return []
+
+start = 0
+end = 1
+split_formula = []
+
+# Loop through characters starting at index 1
+for ch in formula[1:]:
+    if ch.isupper():
+        split_formula.append(formula[start:end])
+        start = end
+    end += 1
+
+# Append the final section
+split_formula.append(formula[start:end])
+
+return split_formula
 
 def split_at_digit(formula):
-    pass  # replace the pass with your code
+digit_location = 1
+for ch in formula[1:]:
+    if ch.isdigit():
+        break
+    digit_location += 1
+if digit_location == len(formula):
+    return formula, 1
+prefix = formula[:digit_location]
+number = int(formula[digit_location:])
+
+return prefix, number
+
 
 def count_atoms_in_molecule(molecular_formula):
     """Takes a molecular formula (string) and returns a dictionary of atom counts.  
